993212410838386e83aaece927a5db1eeb032122
Elizabeth Munoz <munoz_714@live.com>
2020-03-05 23:50:43 -0700

    html format + css + js ids

---
+++ script.js
@@ -0,0 +1,32 @@
+var question = document.getElementById("question");
+var answer = document.getElementById("answer")
+var start = document.getElementById("start");
+var choices = document.getElementById("choices");
+var score = document.getElementById("score");
+var timer = document.getElementById("timer");
+var choiceA = document.getElementById("a");
+var choiceB = document.getElementById("b");
+var choiceC = document.getElementById("c");
+var choiceD = document.getElementById("d");
+
+score = 0;
+var question = [
+    {
+        question: "",
+        choiceA: "",
+        choiceB: "",
+        choiceC: "",
+        choiceD: "",
+        correct: ""
+    },
+    {
+        question: "",
+        choiceA: "",
+        choiceB: "",
+        choiceC: "",
+        choiceD: "",
+        correct: ""
+    },
+
+]
+
