e030f34a9a706a331ca53f699528cbba765a7fc7
Elizabeth Munoz <munoz_714@live.com>
2020-03-08 16:38:06 -0700

    uncommitout question

--- script.js
+++ script.js
@@ -36,98 +36,98 @@ var questions = [
         choiceD: "Character entities have as many parts as is necessary",
         correct: "A character entity has three parts",
     },
-    // {
-    //     question: "Which of the following are used to view documents on the Internet and World Wide Web",
-    //     choiceA: "navigators",
-    //     choiceB: "system software",
-    //     choiceC: "browsers",
-    //     choiceD: "Explorers",
-    //     correct: "browsers",
-    // },
-    // {
-    //     question: "Choose the correct HTML tag for the largest heading",
-    //     choiceA: "the h1 tag",
-    //     choiceB: "the H6 tag",
-    //     choiceC: "the h4 tag",
-    //     choiceD: "the largest tag",
-    //     correct: "the h1 tag",
-    // },
-    // {
-    //     question: "The information that is the visible part of the document goes into this section which begins",
-    //     choiceA: "the head tag",
-    //     choiceB: "the body tag",
-    //     choiceC: "the HTML tag",
-    //     choiceD: "the section tag",
-    //     correct: "the body tag",
-    // },
-    // {
-    //     question: "When specifying the source of an image referenced in your HTML document, the image should",
-    //     choiceA: "either be within the same folder as the HTML page, or the complete path name should be specified.",
-    //     choiceB: "be located on the flashdrive in use",
-    //     choiceC: "be sized apprpriately on the HTML page in order to fit",
-    //     choiceD: "be of a format tht HTML can recognize and manipulate",
-    //     correct: "either be within the same folder as the HTML page, or the complete path name should be specified.",
-    // },
-    // {
-    //     question: "How can you make a list that lists the items with numbers?",
-    //     choiceA: "the list tag",
-    //     choiceB: "the ul tag",
-    //     choiceC: "the ol tag",
-    //     choiceD: "the dl tag",
-    //     correct: "the ol tag",
-    // },
-    // {
-    //     question: "Which of the following means not equalt to in Javascript",
-    //     choiceA: "!= ",
-    //     choiceB: "<>",
-    //     choiceC: "=!",
-    //     choiceD: "not",
-    //     correct: "!=",
-    // },
-    // {
-    //     question: "The variables Numb1 and numb1, are interchangeable in JavaScript",
-    //     choiceA: "True",
-    //     choiceB: "False",
-    //     correct: "False",
-    // },
-    // {
-    //     question: "Where is the correct place to insert a JavaScript",
-    //     choiceA: "The body section",
-    //     choiceB: "the head section",
-    //     choiceC: "it must be inserted in the body and head section",
-    //     choiceD: "it can be inserted in either the body or the head, as it makes no difference",
-    //     correct: "The body section",
-    // },
-    // {
-    //     question: "Which of the following is not a use of JavaScript",
-    //     choiceA: "Browser detection",
-    //     choiceB: "cookies",
-    //     choiceC: "form validation",
-    //     choiceD: "user verification",
-    //     correct: "user verification",
-    // },
-    // {
-    //     question: "Curly Brackets look like this < >",
-    //     choiceA: "True",
-    //     choiceB: "False",
-    //     correct: "False",
-    // },
-    // {
-    //     question: "HTML:",
-    //     choiceA: "Hypertext Markup Language",
-    //     choiceB: "Hyper Type Markup Language",
-    //     choiceC: "Hyperlink Markup Language",
-    //     choiceD: "Hold that microphone loosely",
-    //     correct: "Hypertext Markup Language",
-    // },
-    // {
-    //     question: "Value:",
-    //     choiceA: "is placed before the attribute.",
-    //     choiceB: "defines the value of a website in dollars.",
-    //     choiceC: "is used only in HTML tags.",
-    //     choiceD: "gives meaning to the attribute.",
-    //     correct: "gives meaning to the attribute.",
-    // },
+    {
+        question: "Which of the following are used to view documents on the Internet and World Wide Web",
+        choiceA: "navigators",
+        choiceB: "system software",
+        choiceC: "browsers",
+        choiceD: "Explorers",
+        correct: "browsers",
+    },
+    {
+        question: "Choose the correct HTML tag for the largest heading",
+        choiceA: "the h1 tag",
+        choiceB: "the H6 tag",
+        choiceC: "the h4 tag",
+        choiceD: "the largest tag",
+        correct: "the h1 tag",
+    },
+    {
+        question: "The information that is the visible part of the document goes into this section which begins",
+        choiceA: "the head tag",
+        choiceB: "the body tag",
+        choiceC: "the HTML tag",
+        choiceD: "the section tag",
+        correct: "the body tag",
+    },
+    {
+        question: "When specifying the source of an image referenced in your HTML document, the image should",
+        choiceA: "either be within the same folder as the HTML page, or the complete path name should be specified.",
+        choiceB: "be located on the flashdrive in use",
+        choiceC: "be sized apprpriately on the HTML page in order to fit",
+        choiceD: "be of a format tht HTML can recognize and manipulate",
+        correct: "either be within the same folder as the HTML page, or the complete path name should be specified.",
+    },
+    {
+        question: "How can you make a list that lists the items with numbers?",
+        choiceA: "the list tag",
+        choiceB: "the ul tag",
+        choiceC: "the ol tag",
+        choiceD: "the dl tag",
+        correct: "the ol tag",
+    },
+    {
+        question: "Which of the following means not equalt to in Javascript",
+        choiceA: "!= ",
+        choiceB: "<>",
+        choiceC: "=!",
+        choiceD: "not",
+        correct: "!=",
+    },
+    {
+        question: "The variables Numb1 and numb1, are interchangeable in JavaScript",
+        choiceA: "True",
+        choiceB: "False",
+        correct: "False",
+    },
+    {
+        question: "Where is the correct place to insert a JavaScript",
+        choiceA: "The body section",
+        choiceB: "the head section",
+        choiceC: "it must be inserted in the body and head section",
+        choiceD: "it can be inserted in either the body or the head, as it makes no difference",
+        correct: "The body section",
+    },
+    {
+        question: "Which of the following is not a use of JavaScript",
+        choiceA: "Browser detection",
+        choiceB: "cookies",
+        choiceC: "form validation",
+        choiceD: "user verification",
+        correct: "user verification",
+    },
+    {
+        question: "Curly Brackets look like this < >",
+        choiceA: "True",
+        choiceB: "False",
+        correct: "False",
+    },
+    {
+        question: "HTML:",
+        choiceA: "Hypertext Markup Language",
+        choiceB: "Hyper Type Markup Language",
+        choiceC: "Hyperlink Markup Language",
+        choiceD: "Hold that microphone loosely",
+        correct: "Hypertext Markup Language",
+    },
+    {
+        question: "Value:",
+        choiceA: "is placed before the attribute.",
+        choiceB: "defines the value of a website in dollars.",
+        choiceC: "is used only in HTML tags.",
+        choiceD: "gives meaning to the attribute.",
+        correct: "gives meaning to the attribute.",
+    },
 ]
 var count = 0;
 var questionLength = questions.length;
@@ -149,7 +149,6 @@ choiceD.addEventListener("click", checkAnswer);
 function startQuiz() {
     ticker = setInterval(() => {
         startingTime--;
-        console.log(startingTime)
         timer.innerHTML = "Timer: " + startingTime;
     }, 1000);
 
@@ -202,8 +201,6 @@ function checkAnswer(event) {
 function endGame() {
     quiz.innerHTML = "Your Score: " + score;
     scoreId.style.display = "block";
-    //console.log('this is the score')document.getElementById('score')
-    //
 }
 
 
